0a7866296527e2b6bf45c9efd2c88d5cfd729653
Muhammad Ridwan Hakim, S.T., CPITA, ITPMCP <adminrescen@gmail.com>
2025-04-05 16:58:40 +0700

    Add first migration

---
+++ Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PosMvc.Models
+{
+    public class Role:IdentityRole
+    {
+        public string? Description { get; set; }
+        public virtual ICollection<User>? Users { get; set; }
+        public virtual ICollection<Menu>? Menus { get; set; }
+    }
+}
